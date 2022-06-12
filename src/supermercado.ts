db89fad2c555ffcd1c5fcae9dc484b0b9e4f3f44
Devo-Maidana <104173788+Devo-Maidana@users.noreply.github.com>
2022-06-11 22:09:18 -0300

    .... recuperatorio

--- src/supermercado.ts
+++ src/supermercado.ts
@@ -1,12 +1,11 @@
+//variables que voy a usar en mi programa
 
-//variables que voy a usar en mi programa 
-
-let cantidadProductos:number = Number (prompt("ingrese cantidad de productos"));
-let listaProductos: string [] = new Array(cantidadDeProductos);
-let precioUnitarioProductos: number [] = new Array (cantidadDeProductos);
-let cantidadArticulo: number [] = new Array (cantidadDeProductos);
-let precioTotalCompra:number = 0;
+let cantidadProductos: number = Number(prompt("ingrese cantidad de productos"));
+let listaProductos: string[] = new Array(cantidadDeProductos);
+let precioUnitarioProductos: number[] = new Array(cantidadDeProductos);
+let cantidadArticulo: number[] = new Array(cantidadDeProductos);
+let precioTotalCompra: number = 0;
 //variable para descontar 10%
-let descuento: number =0.1;
-
-//descuentos de la compra 
+let descuento: number = 0.1;
+//
+//descuentos de la compra
